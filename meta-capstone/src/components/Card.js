8b94d1c8a19b3e2b4242d8b9256fa3262b52f894
lettiagc <letti.agc@gmail.com>
2023-11-01 16:12:28 +0100

    created specials section

--- meta-capstone/src/components/Card.js
+++ meta-capstone/src/components/Card.js
@@ -1,18 +1,18 @@
 import Button from "../components/Button";
 
-export default function Card(props) {
+export default function Card({image, title, price, text, href}) {
   return (
     <div className="card">
       <div className="card-header">
-        <img src={props.image}></img>
+        <img src={image}></img>
         <div className="card-header-text">
-          <h3 className="card-title">{props.title}</h3>
-          <span className="card-price">{props.price}</span>
+          <h3 className="card-title">{title}</h3>
+          <span className="card-price">{price}</span>
         </div>
       </div>
       <div className="card-body">
-        <p className="card-text">{props.text}</p>
-        <Button href={props.href} type={"secondary"}>
+        <p className="card-text">{text}</p>
+        <Button href={href} type={"secondary"}>
           Order a delivery
         </Button>
       </div>
--- meta-capstone/src/components/Specials.js
+++ meta-capstone/src/components/Specials.js
@@ -1,11 +1,72 @@
 import Card from "./Card";
+import Salad from "../assets/greek salad.jpg";
+import Bruchetta from "../assets/bruchetta.svg";
+import Lemon from "../assets/lemon dessert.jpg";
+import Button from "./Button";
+
+const specials = [
+  {
+    id: 1,
+    image: Salad,
+    title: "Greek salad",
+    price: "$12.99",
+    text: "The famous greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.",
+    href: "#",
+  },
+  {
+    id: 2,
+    image: Bruchetta,
+    title: "Bruchetta",
+    price: "$5.99",
+    text: "Our Bruchetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil.",
+    href: "#",
+  },
+  {
+    id: 3,
+    image: Lemon,
+    title: "Lemon Dessert",
+    price: "$5.00",
+    text: "This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.",
+    href: "#",
+  },
+  {
+    id: 4,
+    image: Salad,
+    title: "Special 1",
+    price: "$10.99",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    href: "#",
+  },
+  {
+    id: 5,
+    image: Salad,
+    title: "Special 1",
+    price: "$10.99",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    href: "#",
+  },
+];
 
 export default function Specials() {
   return (
     <section className="specials">
-      <h2>Specials</h2>
+      <div className="specials-header">
+        <h2>Specials</h2>
+        <Button href="#" type={"primary"}>
+          Online Menu
+        </Button>
+      </div>
       <div className="specials-grid">
-        <Card />
+        {specials.map((special) => (
+          <Card
+            key={special.id}
+            image={special.image}
+            title={special.title}
+            price={special.price}
+            text={special.text}
+            href={special.href}
+          />
+        ))}
       </div>
     </section>
   );
